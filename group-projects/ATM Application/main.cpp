791998e013697b69b2f3809c256184e1c96da22c
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-22 13:46:41 -0500

    Explanations

--- group-projects/ATM Application/main.cpp
+++ group-projects/ATM Application/main.cpp
@@ -1,7 +1,7 @@
 /*****
-* Author: Glowstudent
-* ATM Application
-*****/
+ * Author: Glowstudent
+ * ATM Application
+ *****/
 
 #include <iostream>
 
@@ -9,6 +9,29 @@ using namespace std;
 
 int main()
 {
+    // ATM Has $500.00
+    // User has $1,000.00
+    // ATM can only dispense $40, $80, $200, and $400 amounts
+    // After withdrawl, the console will show the amount left in the account
+    // And will show how much the ATM has left to dispense
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    //         ATM Application
+    //
+    //      User Balance: $1,000.00
+    //
+    //          Choose One
+    //
+    // | $40                   | $80
+    //
+    // | $200                  | $400
+    //
+    //
+    // EXIT
+    //
+    //    Amount Left in ATM: $500.00
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
     cout << "Hello World!" << endl;
 
     return 0;
--- group-projects/Bad VW Application/main.cpp
+++ group-projects/Bad VW Application/main.cpp
@@ -1,6 +1,6 @@
 /*****
 * Author: Glowstudent
-* Bad VW Application
+* Bad VW Application (VolksWagen)
 *****/
 
 #include <iostream>
@@ -9,6 +9,15 @@ using namespace std;
 
 int main()
 {
+    // A table of 5 VW models, one golf, and one beetle
+    // The table will show the model and the MPG
+
+    // Another 5 vehicles but Chrysler or Fiat models, one Jeep
+    // The table will show the model and the MPG
+
+    // A cell in the table will show the 10% reduction in MPG for each model
+    // Newly calculated honest MPG to the nearest 10th for each model
+
     cout << "Hello World!" << endl;
 
     return 0;
