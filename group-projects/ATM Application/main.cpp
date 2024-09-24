89734b602416821dedfc640b53eb2113a046605f
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-23 22:48:50 -0500

    ATM Program spacing update

--- group-projects/ATM Application/main.cpp
+++ group-projects/ATM Application/main.cpp
@@ -7,7 +7,6 @@
 #include <iomanip>
 #include <string>
 
-
 using namespace std;
 
 int main()
@@ -20,46 +19,40 @@ int main()
     float userBalance = 1000;
     float atmBalance = 500;
 
-    const int sideExtraL = ((screenWidth % 2 == 0 ? screenWidth/2 : (screenWidth + 1)/2) - (to_string(userBalance).length()) + 15);
+    // Math for consistent spacing
+    const int sideL = (((screenWidth - 15 - (to_string(userBalance).length() - 4)) / 2) - (screenWidth % 2 == 0 ? 0 : 1));
+    const int sideR = ((screenWidth - 15 - (to_string(userBalance).length() - 4)) / 2) + ((to_string(userBalance).length() - 4) % 2 == 0 ? 0 : 1);
 
-    // ATM Has $500.00
-    // User has $1,000.00
-    // ATM can only dispense $40, $80, $200, and $400 amounts
-    // After withdrawl, the console will show the amount left in the account
-    // And will show how much the ATM has left to dispense
+// ATM Has $500.00
+// User has $1,000.00
+// ATM can only dispense $40, $80, $200, and $400 amounts
+// After withdrawl, the console will show the amount left in the account
+// And will show how much the ATM has left to dispense
 
-    //*************************************
-    //*          ATM Application          *
-    //*                                   *
-    //*       User Balance: $1,000.00     *
-    //*                                   *
-    //*       How much to withdraw?       *
-    //*                                   *
-    //*   | $40                  | $80    *
-    //*                                   *
-    //*   | $200                 | $400   *
-    //*                                   *
-    //*                                   *
-    //*  EXIT                             *
-    //*                                   *
-    //*    Amount Left in ATM: $500.00    *
-    //*************************************
+// Program will look like this:
+// *****************************************
+// *            ATM Application            *
+// *****************************************
+// *        User Balance: $1000.00         *
+// *                                       *
+// *          Choose One                   *
+// *          | $40          | $80         *
+// *          | $200          | $400       *
+// *                                       *
+// *                                       *
+// *  EXIT                                 *
+// *                                       *
+// *    Amount Left in ATM: $500.00        *
+// *****************************************
 
-    // Clear the screen
-    #if defined(_WIN32) || defined(_WIN64)
-        system("cls");
-    #elif defined(__linux__)
-        system("clear");
-    #elif defined(__APPLE__)
-        system("clear");
-    #endif
-
-    cout << sideExtraL << endl;
-    cout << (screenWidth % 2 == 0 ? screenWidth/2 : (screenWidth + 1)/2) << endl;
-    // int balCommas = (to_string(userBalance).length() - 7) > 3 ? (to_string(userBalance).length() - 7) / 3 : 0;
-
-    
-    cin >> userBalance;
+// Clear the screen
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#elif defined(__linux__)
+    system("clear");
+#elif defined(__APPLE__)
+    system("clear");
+#endif
 
     // Title
     cout << string(screenWidth, '*') << endl;
@@ -67,11 +60,11 @@ int main()
     cout << string(screenWidth, '*') << endl;
 
     // User Balance
-    cout << "*" << setprecision(2) << fixed << setw(sideExtraL) << "User Balance: $" << userBalance << setw((screenWidth / 2) - to_string(userBalance).length()) << "*" << endl;
-    
+    cout << "*" << setprecision(2) << fixed << setw(sideL) << "" << "User Balance: $" << userBalance << setw(sideR) << "*" << endl;
+
     // Spacing
-    cout << "*" << "\n";
-    
+    cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
+
     cout << "*" << string((screenWidth - 20) / 2, ' ') << "Choose One" << endl;
     cout << "*" << string((screenWidth - 20) / 2, ' ') << "| $40" << string((screenWidth - 20) / 2, ' ') << "| $80" << endl;
     cout << "*" << string((screenWidth - 20) / 2, ' ') << "| $200" << string((screenWidth - 20) / 2, ' ') << "| $400" << endl;
