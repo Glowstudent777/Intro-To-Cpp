af6a6383c68a706121e1a9fcd66d21ff28493d37
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-27 11:12:56 -0500

    ATM fully functional

--- group-projects/ATM Application/main.cpp
+++ group-projects/ATM Application/main.cpp
@@ -9,43 +9,9 @@
 
 using namespace std;
 
-int main()
+void clearScreen()
 {
-    // Consistent spacing
-    const string title = "ATM Application";
-    const int screenWidth = title.length() % 2 == 0 ? 40 : 41;
-    const int screenHeight = 20;
-
-    float userBalance = 1000;
-    float atmBalance = 500;
-
-    // Math for consistent spacing
-    const int sideL = (((screenWidth - 15 - (to_string(userBalance).length() - 4)) / 2) - (screenWidth % 2 == 0 ? 0 : 1));
-    const int sideR = ((screenWidth - 15 - (to_string(userBalance).length() - 4)) / 2) + ((to_string(userBalance).length() - 4) % 2 == 0 ? 0 : 1);
-
-// ATM Has $500.00
-// User has $1,000.00
-// ATM can only dispense $40, $80, $200, and $400 amounts
-// After withdrawl, the console will show the amount left in the account
-// And will show how much the ATM has left to dispense
-
-// Program will look like this:
-// *****************************************
-// *            ATM Application            *
-// *****************************************
-// *        User Balance: $1000.00         *
-// *                                       *
-// *          Choose One                   *
-// *      | $40          | $80             *
-// *      | $200          | $400           *
-// *                                       *
-// *                                       *
-// *  EXIT                                 *
-// *                                       *
-// *    Amount Left in ATM: $500.00        *
-// *****************************************
-
-// Clear the screen
+    // Clear the screen
 #if defined(_WIN32) || defined(_WIN64)
     system("cls");
 #elif defined(__linux__)
@@ -53,22 +19,133 @@ int main()
 #elif defined(__APPLE__)
     system("clear");
 #endif
+}
 
-    // Title
+void printScreen(string title, float userBal, float atmBal, string message)
+{
+    const int screenWidth = title.length() % 2 == 0 ? 40 : 41;
+
     cout << string(screenWidth, '*') << endl;
     cout << "*" << setw((screenWidth - title.length()) / 2 - 1) << "" << title << setw((screenWidth - title.length()) / 2) << "*" << endl;
     cout << string(screenWidth, '*') << endl;
 
     // User Balance
-    cout << "*" << setprecision(2) << fixed << setw(sideL) << "" << "User Balance: $" << userBalance << setw(sideR) << "*" << endl;
+    cout << "*" << setprecision(2) << fixed << setw((((screenWidth - 15 - (to_string(userBal).length() - 4)) / 2) - (screenWidth % 2 == 0 ? 0 : 1))) << "" << "User Balance: $" << userBal << setw(((screenWidth - 15 - (to_string(userBal).length() - 4)) / 2) + ((to_string(userBal).length() - 4) % 2 == 0 ? 0 : 1)) << "*" << endl;
 
-    // Spacing
+    // Successfull Transaction
+    cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
+    cout << "*" << setw((screenWidth - message.length()) / 2) << "" << message << setw((screenWidth - message.length()) / 2) << "*" << endl;
     cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
 
     // ATM Withdrawl Options
-    cout << "*" << string((screenWidth - 20) / 2, ' ') << "Choose One" << endl;
-    cout << "*" << string((screenWidth - 20) / 2, ' ') << "| $40" << string((screenWidth - 20) / 2, ' ') << "| $80" << endl;
-    cout << "*" << string((screenWidth - 20) / 2, ' ') << "| $200" << string((screenWidth - 20) / 2, ' ') << "| $400" << endl;
+    // Values don't change so spacing is hardcoded
+    cout << "*" << setw(24) << "Choose One" << setw(16) << "*" << endl;
+    cout << "*" << setw(29) << "| $40          | $80" << setw(11) << "*" << endl;
+    cout << "*" << setw(30) << "| $200         | $400" << setw(10) << "*" << endl;
+
+    // Spacing
+    cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
+    cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
+    cout << "*" << setw(screenWidth - 2) << "" << "*" << endl;
+
+    // ATM Balance
+    cout << "*" << setprecision(2) << fixed << setw(((screenWidth - 21 - (to_string(atmBal).length() - 4)) / 2) - (screenWidth % 2 == 0 ? 0 : 1)) << "" << "Amount Left in ATM: $" << atmBal << setw(((screenWidth - 21 - (to_string(atmBal).length() - 4)) / 2) + ((to_string(atmBal).length() - 4) % 2 == 0 ? 0 : 1)) << "*" << endl;
+    cout << string(screenWidth, '*') << endl;
+}
+
+int main()
+{
+    const string title = "ATM Application";
+    float userBalance = 1000;
+    float atmBalance = 500;
+
+    string message;
+    string userInput;
+
+    // ATM Has $500.00
+    // User has $1,000.00
+    // ATM can only dispense $40, $80, $200, and $400 amounts
+    // After withdrawl, the console will show the amount left in the account
+    // And will show how much the ATM has left to dispense
+
+    // Program will look like this:
+    // *****************************************
+    // *            ATM Application            *
+    // *****************************************
+    // *        User Balance: $1000.00         *
+    // *                                       *
+    // *          Choose One                   *
+    // *      | $40          | $80             *
+    // *      | $200         | $400            *
+    // *                                       *
+    // *                                       *
+    // *                                       *
+    // *                                       *
+    // *    Amount Left in ATM: $500.00        *
+    // *****************************************
+    // Choose An Option:
+
+    // Loop for the ATM to refresh after each transaction
+    while (true)
+    {
+        clearScreen();
+        printScreen(title, userBalance, atmBalance, message);
+        cout << "Choose Amount to Withdrawl or type EXIT to quit: ";
+        getline(cin, userInput);
+
+        while (userInput != "40" && userInput != "80" && userInput != "200" && userInput != "400")
+        {
+            if (userInput == "EXIT")
+            {
+                cout << "Exiting ATM Application..." << endl;
+                exit(0);
+            }
+            else
+            {
+                message = "";
+                clearScreen();
+                printScreen(title, userBalance, atmBalance, message);
+                cout << "Invalid Input. Please enter a valid amount or EXIT." << endl;
+                cout << "Choose Amount to Withdraw or type EXIT to quit: ";
+                getline(cin, userInput);
+            }
+        }
+
+        float amount = stof(userInput);
+
+        while (userBalance < amount || atmBalance < amount)
+        {
+            message = "";
+            clearScreen();
+            printScreen(title, userBalance, atmBalance, message);
+            cout << "Insufficient Funds. Please enter a valid amount or EXIT." << endl;
+            cout << "Choose Amount to Withdraw or type EXIT to quit: ";
+            getline(cin, userInput);
+
+            while (userInput != "40" && userInput != "80" && userInput != "200" && userInput != "400")
+            {
+                if (userInput == "EXIT")
+                {
+                    cout << "Exiting ATM Application..." << endl;
+                    exit(0);
+                }
+                else
+                {
+                    message = "";
+                    clearScreen();
+                    printScreen(title, userBalance, atmBalance, message);
+                    cout << "Invalid Input. Please enter a valid amount or EXIT." << endl;
+                    cout << "Choose Amount to Withdraw or type EXIT to quit: ";
+                    getline(cin, userInput);
+                }
+            }
+            amount = stof(userInput);
+        }
+
+        userBalance -= amount;
+        atmBalance -= amount;
+        message = "Transaction successful!";
+    }
 
     return 0;
 }
