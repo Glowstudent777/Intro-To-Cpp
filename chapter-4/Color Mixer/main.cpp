d7aa626879d4e0b89d26c18490cd909945103147
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-29 00:59:16 -0500

    Color Mixer base

--- chapter-4/Color Mixer/main.cpp
+++ chapter-4/Color Mixer/main.cpp
@@ -1,14 +1,72 @@
 /*****
-* Author: Glowstudent
-* Color Mixer
-*****/
+ * Author: Glowstudent
+ * Color Mixer
+ *****/
 
 #include <iostream>
+#include <string>
+#include <limits>
 
 using namespace std;
 
+void clearScreen()
+{
+    // Clear the screen
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#elif defined(__linux__)
+    system("clear");
+#elif defined(__APPLE__)
+    system("clear");
+#endif
+}
+
 int main()
 {
+    string color1, color2;
+
+    cout << "This program mixes two primary colors.\n";
+
+    cout << "Enter a primary color (red, blue, yellow): ";
+    getline(cin, color1);
+
+    while (color1 != "red" && color1 != "blue" && color1 != "yellow")
+    {
+        clearScreen();
+        cout << "Error: Invalid color. Please enter a primary color (red, blue, yellow): ";
+        getline(cin, color1);
+    }
+
+    cout << "Enter another primary color: ";
+    cin >> color2;
+
+    while (color2 != "red" && color2 != "blue" && color2 != "yellow")
+    {
+        clearScreen();
+        cout << "Error: Invalid color. Please enter a primary color (red, blue, yellow): ";
+        cin >> color2;
+    }
+
+    if (color1 == color2)
+    {
+        cout << "The mixed color is " << color1 << ".\n";
+    }
+    else if (color1 == "red" && color2 == "blue" || color1 == "blue" && color2 == "red")
+    {
+        cout << "The mixed color is purple.\n";
+    }
+    else if (color1 == "red" && color2 == "yellow" || color1 == "yellow" && color2 == "red")
+    {
+        cout << "The mixed color is orange.\n";
+    }
+    else if (color1 == "blue" && color2 == "yellow" || color1 == "yellow" && color2 == "blue")
+    {
+        cout << "The mixed color is green.\n";
+    }
+    else
+    {
+        cout << "Error: Invalid colors.\n";
+    }
 
     return 0;
 }
