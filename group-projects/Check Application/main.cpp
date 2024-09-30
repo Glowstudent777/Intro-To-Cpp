6397aa391e26499eae81b6e67b05625f719fe81c
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-30 12:24:43 -0500

    Fix PayCheck spacing

--- group-projects/Check Application/main.cpp
+++ group-projects/Check Application/main.cpp
@@ -1,9 +1,9 @@
 /*****
-* Author: Glowstudent
-* Check Application
-* Note: Run with C++ 20
-* Spacing might be off depending on if your compiler shows commas with cout.imbue(locale("")); Onlines GDB does but my local compiler does not.
-*****/
+ * Author: Glowstudent
+ * Check Application
+ * Note: Run with C++ 20
+ * Spacing might be off depending on if your compiler shows commas with cout.imbue(locale("")); Onlines GDB does but my local compiler does not.
+ *****/
 
 #include <iostream>
 #include <iomanip>
@@ -12,15 +12,26 @@
 
 using namespace std;
 
+int getCommas(double pay)
+{
+	int commas = 0;
+	while (pay > 1000)
+	{
+		pay /= 1000;
+		commas++;
+	}
+	return commas;
+}
+
 int main()
 {
 	/*****
-	* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	* >                               | $1,000,000 |  >
-	* >                                               >
-	* > ___Pay to the Order of___     Johnny PayCheck >
-	* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	*****/
+	 * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	 * >                               | $1,000,000 |  >
+	 * >                                               >
+	 * > ___Pay to the Order of___     Johnny PayCheck >
+	 * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	 *****/
 
 	string employee, beforeName = "___Pay to the Order of___";
 	double pay;
@@ -35,15 +46,17 @@ int main()
 	cout << "\n";
 
 	// 7 is the length of the string ".000000"
-	int payCommas = (to_string(pay).length() - 7) > 3 ? (to_string(pay).length() - 7) / 3 : 0;
+	int payCommas = getCommas(pay);
+	// int payCommas = (to_string(pay).length() - 7) / 3;
 
 	// Fun math to figure out the extra spacing needed to make the check look nice
 	// +9 is the length of the string " PayCheck" including the space
 	// | $300.00 | is + 1 because -4 for the hidden 0s but +5 for the text surrounding 300: | $300 |
 	int payEndExtra = (employee.length() + 9) < (to_string(pay).length() + 1 + payCommas) ? endSpacing : (endSpacing + (employee.length() + 9 - (to_string(pay).length() + 1 + payCommas)));
 	int employeeEndExtra = (employee.length() + 9) > (to_string(pay).length() + 1 + payCommas) ? endSpacing : (endSpacing + (to_string(pay).length() + 1 + payCommas - (employee.length() + 9)));
-	int midEndExtra = (payEndExtra > employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : (payEndExtra < employeeEndExtra) ? (to_string(pay).length() + 1 + payEndExtra) : (payEndExtra == employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : endSpacing;
-	int topNbottom = (spacing - letters + topNbottom + payCommas + beforeName.length() + endSpacing + ((payEndExtra > employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : (payEndExtra < employeeEndExtra) ? (to_string(pay).length() + 1 + payEndExtra) : (payEndExtra == employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : endSpacing));
+
+	int midEndExtra = (payEndExtra > employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : (payEndExtra < employeeEndExtra) ? (to_string(pay).length() + 1 + payEndExtra) : (payEndExtra == employeeEndExtra)  ? (employee.length() + 9 + employeeEndExtra) : endSpacing;
+	int topNbottom = (spacing - letters + payCommas + topNbottom + beforeName.length() + endSpacing + ((payEndExtra > employeeEndExtra) ? (employee.length() + 9 + employeeEndExtra) : (payEndExtra < employeeEndExtra) ? (to_string(pay).length() + 1 + payEndExtra) : (payEndExtra == employeeEndExtra)	? (employee.length() + 9 + employeeEndExtra) : endSpacing));
 
 	cout.imbue(locale(""));
 	cout << string(topNbottom, '>') << endl;
