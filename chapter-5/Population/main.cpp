621798f86b1967ace23e8e8784d001de55509fa0
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-10-18 20:35:04 -0500

    Population and The Greatest and Least of These

--- chapter-5/Population/main.cpp
+++ chapter-5/Population/main.cpp
@@ -1,21 +1,68 @@
 /*****
-* Author: Glowstudent
-* Program: Template
-*****/
+ * Author: Glowstudent
+ * Program: Population
+ *****/
 
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
-    string name;
-    
-    cout << "Enter your name: ";
-    getline(cin, name);
+    int startingPopulation, averageDailyIncrease, days;
 
-    cout << "Hello, " << name << "!" << endl;
+    cout << "Enter the starting population: ";
+    cin >> startingPopulation;
 
-	return 0;
+    while (startingPopulation < 2 || cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "The starting population must be at least 2." << endl;
+        cout << "Enter the starting population: ";
+        cin >> startingPopulation;
+    }
+
+    cout << "Enter the average daily population increase (as a percentage): ";
+    cin >> averageDailyIncrease;
+
+    while (averageDailyIncrease < 0 || cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "The average daily population increase must be at least 0." << endl;
+        cout << "Enter the average daily population increase (as a percentage): ";
+        cin >> averageDailyIncrease;
+    }
+
+    cout << "Enter the number of days to multiply: ";
+    cin >> days;
+
+    while (days < 1 || cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "The number of days must be at least 1." << endl;
+        cout << "Enter the number of days: ";
+        cin >> days;
+    }
+
+    cout << endl;
+
+    cout << "Day\tApproximate Population" << endl;
+    cout << "------------------------------" << endl;
+
+    cout << fixed << setprecision(0);
+    for (int i = 1; i <= days; i++)
+    {
+        cout << i << "\t\t" << startingPopulation << endl;
+        startingPopulation += startingPopulation * averageDailyIncrease / 100;
+    }
+
+    return 0;
 }
--- chapter-5/The Greatest and Least of These/main.cpp
+++ chapter-5/The Greatest and Least of These/main.cpp
@@ -1,21 +1,63 @@
 /*****
-* Author: Glowstudent
-* Program: Template
-*****/
+ * Author: Glowstudent
+ * Program: The Greatest and Least of These
+ *****/
 
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
-    string name;
+    int smallest, largest, input;
+    bool first = true;
+
+    do
+    {
+        if (first)
+            cout << "Enter an integer (-99 to end): ";
+        else
+            cout << "Enter another integer (-99 to end): ";
+
+        cin >> input;
+
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Invalid input. Please Enter an integer (-99 to end): ";
+            cin >> input;
+        }
+
+        switch (input)
+        {
+        case -99:
+            break;
+        default:
+            if (input < smallest || first)
+                smallest = input;
+            if (input > largest || first)
+                largest = input;
+
+            first = false;
+            break;
+        }
+
+    } while (input != -99);
+
+    cout << endl;
+
+    if (first)
+    {
+        smallest = 0;
+        largest = 0;
+    }
     
-    cout << "Enter your name: ";
-    getline(cin, name);
+    cout << "Smallest: " << smallest << endl;
+    cout << "Largest: " << largest << endl;
 
-    cout << "Hello, " << name << "!" << endl;
-
-	return 0;
+    return 0;
 }
