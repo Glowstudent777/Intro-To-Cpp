3b91702207495322b964ef3bf5159a77f1628bb3
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-12-02 12:04:25 -0600

    Finish Module 7 stuff

--- chapter-7/Name Search/main.cpp
+++ chapter-7/Name Search/main.cpp
@@ -1,21 +1,132 @@
 /*****
-* Author: Glowstudent
-* Program: Name Search
-*****/
+ * Author: Glowstudent
+ * Program: Name Search
+ *****/
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void clearScreen()
+{
+    // Clear the screen
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#elif defined(__linux__)
+    system("clear");
+#elif defined(__APPLE__)
+    system("clear");
+#endif
+}
+
+string toUpper(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+
+    return str;
+}
+
 int main()
 {
-    string name;
-    
-    cout << "Enter your name: ";
-    getline(cin, name);
+    vector<string> boyNames;
+    vector<string> girlNames;
+    string s, name, temp;
+    bool popBoy = false, popGirl = false;
+    ifstream boyFile, girlFile;
 
-    cout << "Hello, " << name << "!" << endl;
+    boyFile.open("BoyNames.txt");
+    girlFile.open("GirlNames.txt");
 
-	return 0;
+    if (!boyFile)
+    {
+        cout << "Error opening BoyNames.txt\n";
+        return 1;
+    }
+
+    if (!girlFile)
+    {
+        cout << "Error opening GirlNames.txt\n";
+        return 1;
+    }
+
+    while (getline(boyFile, s) && !boyFile.eof())
+    {
+        if (s == "")
+            continue;
+
+        boyNames.push_back(s);
+    }
+    while (getline(girlFile, s) && !girlFile.eof())
+    {
+        if (s == "")
+            continue;
+
+        girlNames.push_back(s);
+    }
+
+    boyFile.close();
+    girlFile.close();
+
+    while (true)
+    {
+        clearScreen();
+
+        cout << "Enter a name to search for (exit to exit): ";
+        getline(cin, s);
+
+        while (s == "" || s == " ")
+        {
+            cout << "Please enter a name: ";
+            getline(cin, s);
+        }
+
+        if (toUpper(s) == "EXIT")
+            break;
+
+        for (int i = 0; i < boyNames.size(); i++)
+        {
+            if (popBoy && popGirl)
+                break;
+
+            if (toUpper(s) == toUpper(boyNames[i]))
+            {
+                popBoy = true;
+                name = boyNames[i];
+            }
+
+            if (toUpper(s) == toUpper(girlNames[i]))
+            {
+                popGirl = true;
+                name = girlNames[i];
+            }
+        }
+
+        if (popBoy && popGirl)
+            cout << name << " is a popular name for both boys and girls.\n";
+        else if (popBoy)
+            cout << name << " is a popular boy name.\n";
+        else if (popGirl)
+            cout << name << " is a popular girl name.\n";
+        else
+            cout << name << " is not a popular name.\n";
+
+        popBoy = false;
+        popGirl = false;
+        name = "";
+        s = "";
+
+        cout << endl;
+        cout << "Press Enter to continue...";
+        getline(cin, temp);
+
+        temp = "";
+    }
+
+    return 0;
 }
--- chapter-7/National Commerce Bank/main.cpp
+++ chapter-7/National Commerce Bank/main.cpp
@@ -4,18 +4,43 @@
  *****/
 
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
+bool testPIN(const vector<int>, const vector<int>, int);
+
 int main()
 {
-    string name;
+    const int NUM_DIGITS = 7;
+    vector<int> pin1 = {2, 4, 1, 8, 7, 9, 0};
+    vector<int> pin2 = {2, 4, 6, 8, 7, 9, 0};
+    vector<int> pin3 = {1, 2, 3, 4, 5, 6, 7};
 
-    cout << "Enter your name: ";
-    getline(cin, name);
+    if (testPIN(pin1, pin2, NUM_DIGITS))
+        cout << "Error: pin1 and pin2 report to be the same.\n";
+    else
+        cout << "Success: pin1 and pin2 are different.\n";
 
-    cout << "Hello, " << name << "!" << endl;
+    if (testPIN(pin1, pin3, NUM_DIGITS))
+        cout << "Error: pin1 and pin3 report to be the same.\n";
+    else
+        cout << "Success: pin1 and pin3 are different.\n";
+
+    if (testPIN(pin1, pin1, NUM_DIGITS))
+        cout << "Success: pin1 and pin1 report to be the same.\n";
+    else
+        cout << "Error: pin1 and pin1 are different.\n";
 
     return 0;
+}
+
+bool testPIN(const vector<int> custPIN, const vector<int> databasePIN, int size)
+{
+    for (int index = 0; index < size; index++)
+    {
+        if (custPIN[index] != databasePIN[index])
+            return false;
+    }
+    return true;
 }
