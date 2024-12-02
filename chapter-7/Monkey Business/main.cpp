f8ca00ebe9c9effd6d646c172861623f6b2bd70a
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-12-01 19:15:16 -0600

    Two more for Module 7

--- chapter-7/Monkey Business/main.cpp
+++ chapter-7/Monkey Business/main.cpp
@@ -1,21 +1,70 @@
 /*****
-* Author: Glowstudent
-* Program: Monkey Business
-*****/
+ * Author: Glowstudent
+ * Program: Monkey Business
+ *****/
 
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    string name;
-    
-    cout << "Enter your name: ";
-    getline(cin, name);
+    double data[3][5], average = 0, total = 0;
+    int lowMonkey = -1, highMonkey = -1;
+    double low = -1, high = -1;
 
-    cout << "Hello, " << name << "!" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter the data for monkey " << i + 1 << endl;
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "Enter pounds of food eaten for day " << j + 1 << ": ";
+            cin >> data[i][j];
 
-	return 0;
+            while (data[i][j] < 0 || data[i][j] < 0)
+            {
+                cout << "Invalid input. Please enter a positive number: ";
+                cin >> data[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            total += data[i][j];
+            average += data[i][j];
+
+            if (low == -1 && high == -1)
+            {
+                low = data[i][j];
+                lowMonkey = i;
+
+                high = data[i][j];
+                highMonkey = i;
+            }
+
+            if (data[i][j] < low)
+            {
+                low = data[i][j];
+                lowMonkey = i;
+            }
+
+            if (data[i][j] > high)
+            {
+                high = data[i][j];
+                highMonkey = i;
+            }
+        }
+    }
+
+    cout << endl;
+    cout << setprecision(2) << fixed;
+    cout << "The average amount of food eaten by all monkeys per day: " << average / 15 << endl;
+    cout << "The least amount of food eaten during the week is " << low << " by monkey " << (lowMonkey + 1) << endl;
+    cout << "The most amount of food eaten during the week is " << high << " by monkey " << (highMonkey + 1) << endl;
+
+    return 0;
 }
--- chapter-7/Payroll/main.cpp
+++ chapter-7/Payroll/main.cpp
@@ -1,21 +1,69 @@
 /*****
-* Author: Glowstudent
-* Program: Payroll
-*****/
+ * Author: Glowstudent
+ * Program: Payroll
+ *****/
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
+bool isNumber(const std::string &s)
+{
+    return !s.empty() && s.find_first_not_of("-.0123456789") == std::string::npos;
+}
+
 int main()
 {
-    string name;
-    
-    cout << "Enter your name: ";
-    getline(cin, name);
+    const long int empId[7] = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489};
+    const double minPayRate = 15.00;
+    int hours[7];
+    double payRate[7], wages[7];
 
-    cout << "Hello, " << name << "!" << endl;
+    for (int i = 0; i < 7; i++)
+    {
+        string s;
+        int tempHours;
+        double tempPayRate;
 
-	return 0;
+        cout << "Enter the hours worked by employee " << empId[i] << ": ";
+        getline(cin, s);
+
+        while (s.empty() || !isNumber(s) || stoi(s) < 0)
+        {
+            cout << "Invalid input. Please enter a positive number: ";
+            getline(cin, s);
+        }
+
+        tempHours = stoi(s);
+
+        cout << "Enter the pay rate for employee " << empId[i] << ": ";
+        getline(cin, s);
+
+        while (s.empty() || !isNumber(s) || stod(s) < minPayRate)
+        {
+            cout << "Invalid input. Please enter a positive number greater than or equal to " << minPayRate << ": ";
+            getline(cin, s);
+        }
+
+        tempPayRate = stod(s);
+
+        hours[i] = (int)tempHours;
+        payRate[i] = tempPayRate;
+
+        wages[i] = tempHours * tempPayRate;
+    }
+
+    cout << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Employee ID\tHours Worked\tPay Rate\tWages" << endl;
+    cout << "--------------------------------------------------------" << endl;
+    for (int i = 0; i < 7; i++)
+    {
+        cout << empId[i] << "\t\t" << hours[i] << "\t\t$" << payRate[i] << "\t\t$" << wages[i] << endl;
+    }
+    cout << endl;
+
+    return 0;
 }
